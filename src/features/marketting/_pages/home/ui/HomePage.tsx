631fc5d7b03a6db7e5372a601f357791c5d79b82
tofaal9152 <tofaal91522@gmail.com>
2026-04-29 15:16:18 +0600

    demo chnage to trigger

--- src/features/marketting/_pages/home/ui/HomePage.tsx
+++ src/features/marketting/_pages/home/ui/HomePage.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
     return (
         <div>
-            JENKINS +AWS +UBUNTU +GITHUB CI/CD
+            JENKINS +AWS +UBUNTU +GITHUB CI/CD + start
         </div>
     );
 }
