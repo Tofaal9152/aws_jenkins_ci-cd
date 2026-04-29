e62d0e1c42391bef37a9348262e7a3c08ca6670f
tofaal9152 <tofaal91522@gmail.com>
2026-04-29 17:21:04 +0600

    docker done

--- src/features/marketting/_pages/home/ui/HomePage.tsx
+++ src/features/marketting/_pages/home/ui/HomePage.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
     return (
         <div>
-            JENKINS +AWS +UBUNTU +GITHUB CI/CD PIPELINE USING Docker 
+            JENKINS +AWS +UBUNTU +GITHUB CI/CD PIPELINE USING Docker 111
         </div>
     );
 }
